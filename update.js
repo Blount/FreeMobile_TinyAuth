1e77c2f78ff90dcd59af172d8b089547688aa306
Blount <blount@ilatumi.org>
2015-09-02 14:22:27 +0200

    fichier de suivi de mise à jour

---
+++ update.js
@@ -0,0 +1,8 @@
+// ==UserScript==
+// @name           FreeMobile TinyAuth
+// @namespace      freemobile
+// @description    Retour à l'ancien mode d'identification sur FreeMobile.
+// @include        https://mobile.free.fr/*
+// @updateURL      https://raw.githubusercontent.com/Blount/FreeMobile_TinyAuth/master/update.js
+// @version        2.7.1
+// ==/UserScript==
